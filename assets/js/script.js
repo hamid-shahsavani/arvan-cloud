04a5984ca9f7b8443eb162a968a5e7247d57a4ca
sys113 <051.SYS113@gmail.com>
2022-04-07 18:57:33 +0430

    add sub-menu for main menu chillds

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
+
+//? show language menu 
+
 $(document).ready(function(){
     $('.active-lang').on('click', function(){
         $('.other').toggleClass('show');
     });
 });
+
+//? hide sub-menu 
+
+$(document).ready(function(){
+    $('.sub-menu .item-1').hide();
+    $('.sub-menu .item-2').hide();
+    $('.sub-menu .item-4').hide();
+    $('.sub-menu .item-5').hide();
+    $('.main-menu .item-1').click(function() {
+        $('.sub-menu .item-1').slideToggle("slow");
+    });
+});
+
