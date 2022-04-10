cefe0c0f52bd2463d72fdd70fd9944780dec57f8
sys113 <051.SYS113@gmail.com>
2022-04-10 21:17:00 +0430

    add pretiier and sub-menu base and sub-menu item-1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,64 @@
+//? hide and show element with click function
 
-//? show language menu 
+function hideAndShow(show, active) {
+  $(show).hide();
+  $(active).on('click', function () {
+    if ($(active).hasClass('active')) {
+      $(active).removeClass('active');
+      $(show).slideUp();
+      return 0;
+    }
+    $(active).addClass('active');
+    $(show).slideDown();
+  });
+}
 
-$(document).ready(function(){
-    $('.active-lang').on('click', function(){
-        $('.other').toggleClass('show');
+$(document).ready(function () {
+
+  //* for desktop
+
+  //? show language menu in desktop
+
+  $('.active-lang').on('click', function () {
+    $('.other').toggleClass('show');
+  });
+
+  //? show and hide sub-menu in desktop
+
+  $('.nav-content').hide();
+  $('.nav-tab').on('click', function () {
+    let clickedId = $(this).attr('id');
+    let clickedIdContent = clickedId.replace('-tab', '');
+    if ($('#' + clickedId).hasClass('active')) {
+      $('#' + clickedId).removeClass('active');
+      $('#' + clickedIdContent).slideUp('slow');
+      return 0;
+    }
+    $('.nav-tab').each(function (index) {
+      if ($(this).attr('id') == clickedId) {
+        $('.nav-tab,.nav-content').eq(index).addClass('active');
+        $('.nav-content').eq(index).slideDown();
+      } else {
+        $('.nav-tab,.nav-content').eq(index).removeClass('active');
+        $('.nav-content').eq(index).hide();
+      }
     });
-});
+  });
 
-//? show and hide sub-menu 
+  //* for mobile
+  
+  //? show language menu in mobile
 
-$(document).ready(function () { 
-    $('.nav-content').hide();
-    $('.nav-tab').on("click", function () {
-        let clickedId = $(this).attr("id");
-        let clickedIdContent = clickedId.replace('-tab','');
-        if ($("#"+clickedId).hasClass('active')){
-            $("#"+clickedIdContent).slideUp("slow");
+  hideAndShow('.m-change-lang-sub', '.m-change-lang');
 
-            $("#"+clickedId).removeClass('active');
-            return 0;
-        }
-        $('.nav-tab').each(function (index) {
-            if ($(this).attr("id") == clickedId) {
-                $('.nav-tab,.nav-content').eq(index).addClass('active');
-                $(".nav-content").eq(index).slideDown();
-            } else {
-                $('.nav-tab,.nav-content').eq(index).removeClass('active');
-                $(".nav-content").eq(index).hide();
-            }
-        });
-    });  
+  //? add active class to mobile menu icon and show main-menu
+
+  hideAndShow('.mobile-header .m-show-menu', '.mobile-menu-button input');
+
+  //? add active class to mobile menu item and show sub-menu
+
+  for (let i = 1; i <=4 ; i++) {
+    hideAndShow('.m-main-menu .item-'+i+' > .m-sub-menu' , '.m-main-menu .item-'+i);
+  }
+
 });
