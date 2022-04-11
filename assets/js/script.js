5d42f188ee71d5f735177c06126dc4a6c434cb33
sys113 <051.SYS113@gmail.com>
2022-04-11 18:06:58 +0430

    fix show sub-menu bug and finish develop header menu in mobile and desktop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 //? hide and show element with click function
 
 function hideAndShow(show, active) {
@@ -12,23 +13,9 @@ function hideAndShow(show, active) {
   });
 }
 
-function showSubMenu(subMenu,menu){
-  $(subMenu).hide();
-  let myVar = 0;
-  $(menu).on('click',function(){
-    $(menu).find("ul").click(function(){
-      $(subMenu).slideDown();
-      myVar = 1;
-    });
-    if(myVar) {
-      return
-    } 
-    $(subMenu).slideToggle();
-  });
-}
+//? run ready function
 
 $(document).ready(function () {
-
   //* for desktop
 
   //? show language menu in desktop
@@ -40,6 +27,7 @@ $(document).ready(function () {
   //? show and hide sub-menu in desktop
 
   $('.nav-content').hide();
+
   $('.nav-tab').on('click', function () {
     let clickedId = $(this).attr('id');
     let clickedIdContent = clickedId.replace('-tab', '');
@@ -60,7 +48,7 @@ $(document).ready(function () {
   });
 
   //* for mobile
-  
+
   //? show language menu in mobile
 
   hideAndShow('.m-change-lang-sub', '.m-change-lang');
@@ -71,12 +59,9 @@ $(document).ready(function () {
 
   //? add active class to mobile menu item and show sub-menu
 
-  for (let i = 1; i <=4 ; i++) {
+  for (let i = 1; i <= 4; i++) {
 
-    //! bug ---> click to sub-menu and slideUp and hide sub-menu !
-
-    // hideAndShow('.m-main-menu .item-'+i+' > .m-sub-menu' , '.m-main-menu .item-'+i);
-    hideAndShow('.item-'+i+' > .m-sub-menu' , '.item-'+i);
+    hideAndShow('.m-sub-menu-' + i , '.m-item-' + i);
   }
 
 });
